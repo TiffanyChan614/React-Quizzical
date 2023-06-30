a0d0d1c2a6452b952f4b1c1b6eb90a8e54bab1f1
TiffChan <tiffanychan1999614@gmail.com>
2023-06-30 18:35:57 -0400

    Tidy up folders
    Place assets and components into src for best practices. Import images with import statement to increase code readability and to fix the problem that dynamic string cannot be used as the value for src.

--- react/SoloProject3/React-quizzical/src/App.jsx
+++ react/SoloProject3/React-quizzical/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
-import StartPage from '../components/StartPage'
+import StartPage from './components/StartPage'
+import yellowblob0 from './assets/yellowblob0.svg'
+import yellowblob1 from './assets/yellowblob1.svg'
+import yellowblob2 from './assets/yellowblob2.svg'
+import blueblob0 from './assets/blueblob0.svg'
+import blueblob1 from './assets/blueblob1.svg'
+import blueblob2 from './assets/blueblob2.svg'
 
 const START_PAGE = 0,
 	QUIZ_PAGE = 1,
@@ -14,16 +20,19 @@ function App() {
 
 	console.log('currentPage', currentPage)
 
+	const yellowBlobs = [yellowblob0, yellowblob1, yellowblob2]
+	const blueBlobs = [blueblob0, blueblob1, blueblob2]
+
 	return (
 		<main>
 			<img
 				className='yellow-blob'
-				src={`../asset/yellowblob${currentPage}.svg`}
+				src={yellowBlobs[currentPage]}
 				alt='Yellow blob'
 			/>
 			<img
 				className='blue-blob'
-				src={`../asset/blueblob${currentPage}.svg`}
+				src={blueBlobs[currentPage]}
 				alt='Blue blob'
 			/>
 			{currentPage === START_PAGE && <StartPage handleClick={handleClick} />}
